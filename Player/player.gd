9ab0224667a3a6b1b4ccae4d782dcc3adf1f4b09
CodeDoes <codedoes@gmail.com>
2026-02-19 20:00:00 +0200

    more accel, less float

--- Player/player.gd
+++ Player/player.gd
@@ -1,8 +1,9 @@
 extends Unit
 class_name Player
 
-var move_speed = 500.0
-var move_accel = move_speed *2.0
+@export var move_speed = 500.0
+@export var accel_mult = 4.0
+
 static var instance:Player
 var iframe_max = 1.0
 var iframe = 0.0
@@ -28,6 +29,9 @@ func _physics_process(delta: float) -> void:
 		if iframe<=0:
 			iframe_elapse.emit()
 	var mvec = Input.get_vector("move_left","move_right","move_up","move_down")
-	var desired_velocity = (mvec * move_speed - linear_velocity).limit_length(move_speed)/move_speed * move_accel
+	var desired_velocity = mvec * move_speed
+	desired_velocity = desired_velocity - linear_velocity
+	desired_velocity = desired_velocity.limit_length(move_speed) / move_speed
+	desired_velocity = desired_velocity * move_speed * accel_mult
 	apply_central_force(desired_velocity * mass)
 	super(delta)
