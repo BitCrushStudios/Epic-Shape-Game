730e82eac27d6a2819c8791cb02ebd5022233ada
CodeDoes <codedoes@gmail.com>
2026-02-22 01:22:31 +0200

    menu

--- Scenes/Menu/MainMenu/main_menu.gd
+++ Scenes/Menu/MainMenu/main_menu.gd
@@ -12,8 +12,30 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
+	var tween = create_tween()
+	(tween.tween_property(
+		$Camera2D, 
+		'global_position', 
+		$Control.global_position, 
+		1.0
+	).set_ease(Tween.EASE_IN_OUT)
+	.set_trans(Tween.TRANS_CUBIC))
+	await tween.finished
+	
 	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	var tween = create_tween()
+	tween.tween_property($Camera2D, 'global_position', $SettingContainer.global_position, 1.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	
+
+
+func _on_settings_back_pressed() -> void:
+	var tween = create_tween()
+	tween.tween_property($Camera2D, 'global_position', $MainContainer.global_position, 1.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	
