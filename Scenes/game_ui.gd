040f59c0953ae265a51533d1801a3c8c426d47a4
CodeDoes <codedoes@gmail.com>
2026-02-22 02:57:12 +0200

    game ui node rename

--- Scenes/game_ui.gd
+++ Scenes/game_ui.gd
@@ -2,13 +2,14 @@ extends CanvasLayer
 func _ready():
 	Player.instance.recieved_damage.connect(_recieved_damage)
 	%HurtOverlay.modulate = Color.TRANSPARENT
+	%HurtOverlay.show()
 func _process(_delta:float):
 	if not Player.instance:
 		return
-	%ProgressBar.max_value = Player.instance.health_max
-	%ProgressBar.value = Player.instance.health_current
+	%HealthBar.max_value = Player.instance.health_max
+	%HealthBar.value = Player.instance.health_current
 	print(Player.instance.health_current)
-	%ProgressLabel.text = "%d / %d" % [Player.instance.health_current, Player.instance.health_max]
+	%HealthLabel.text = "%d / %d" % [Player.instance.health_current, Player.instance.health_max]
 var hurt_tween:Tween
 func _recieved_damage(_damage:float):
 	if hurt_tween:
