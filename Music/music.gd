7b53ce921b5f8516ed1b09e005e3d3e0b84cf0ab
CodeDoes <codedoes@gmail.com>
2026-02-23 13:02:57 +0200

    music script fix

--- Music/music.gd
+++ Music/music.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const tracks = [
-	preload("res://Assets/Music/musinova-hyper-garden-jungle-breakbeat-drum-and-bass-loop-edit-356528.mp3"),
-	preload("res://Assets/Music/musinova-technology-content-electronic-ambient-techno-loopable-edit-470305.mp3")
+	#preload("res://Assets/Music/musinova-hyper-garden-jungle-breakbeat-drum-and-bass-loop-edit-356528.mp3"),
+	#preload("res://Assets/Music/musinova-technology-content-electronic-ambient-techno-loopable-edit-470305.mp3")
 ]
 var track_index = 0
 
