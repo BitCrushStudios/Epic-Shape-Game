59e1afb47b59c7c5b4cdfb52a08e3c30ba3e3f8c
Claytomesh <139574216+BitCrushStudios@users.noreply.github.com>
2026-02-24 20:47:22 -0600

    enemy types

--- Game/Units/Enemies/enemy_manager.gd
+++ Game/Units/Enemies/enemy_manager.gd
@@ -1,18 +1,18 @@
 extends Node
 class_name EnemyManager
-func wave1():
-	await spawnOverTime(preload("./EnemyTriangle.tscn"), 10, 3)
-	await waitForTimeOrClear(20)
-func spawnOverTime(tscn:PackedScene, count: int, overTime: float):
-	var tween = create_tween()
-	tween.tween_callback(
-		func ():
-			var spawnPoint: SpawnPoint = preload("./SpawnPoint.tscn").instantiate()
-			
-	)
-	tween.set_loops(count)
+#func wave1():
+	#await spawnOverTime(preload("./EnemyTriangle.tscn"), 10, 3)
+	#await waitForTimeOrClear(20)
+#func spawnOverTime(tscn:PackedScene, count: int, overTime: float):
+	#var tween = create_tween()
+	#tween.tween_callback(
+		#func ():
+			#var spawnPoint: SpawnPoint = preload("./SpawnPoint.tscn").instantiate()
+			#
+	#)
+	#tween.set_loops(count)
 func _process(_delta: float) -> void:
-	if get_child_count() < 10:
+	if get_child_count() < 1:
 		var spawnPoint: SpawnPoint = preload("./SpawnPoint.tscn").instantiate()
 		add_child(spawnPoint,true)
 		var map_rid = get_viewport().world_2d.navigation_map
