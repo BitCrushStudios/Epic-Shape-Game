d84413366e6fbd277db8efe793c52c7e1fb8ec4a
CodeDoes <codedoes@gmail.com>
2026-03-01 00:52:20 +0200

    removing debug ui

--- Game/Ui/Debug/debug_ui.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _process(_delta:float):
-	if not Player.instance:
-		return
-	%PlayerVelocity.text = "%.2f" % Player.instance.linear_velocity.length()
-	%PlayerHealth.text = "%.2f" % Player.instance.health_current
