22f11464113f4a742b289dd58f07fe547e8d3eb4
Claytomesh <139574216+BitCrushStudios@users.noreply.github.com>
2026-03-14 18:54:32 -0500

    hits

--- Game/Weapons/weapon.gd
+++ Game/Weapons/weapon.gd
@@ -60,7 +60,8 @@ func _physics_process(_delta: float) -> void:
 	
 	$NavigationObstacle2D.radius =  max_obstacle_radius if activation_value>0 else 0.0
 		
-func _on_body_entered(body:Node2D):
+func _on_body_entered(body:Node):
+	$"Basic Hit".play()
 	if %ActivatedSprite != null:
 		%ActivatedSprite.play("Hit")
 	
