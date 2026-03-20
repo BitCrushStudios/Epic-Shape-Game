50dc1de9dc947f761f4926793685ec2634e1b5a1
Claytomesh <139574216+BitCrushStudios@users.noreply.github.com>
2026-03-19 20:51:26 -0500

    items

--- Game/Items/ItemUpgradeFist.gd
+++ Game/Items/ItemUpgradeFist.gd
@@ -6,9 +6,9 @@ func _init():
 	super()
 	name = "Fist"
 	texture = preload("res://Assets/Art/Items/Fist Item Icon.png")
-	base_price = 20
+	base_price = -10
 func apply(gameMain:GameMain):
-	gameMain.weaponsManager.add_weapon(preload("res://Game/Weapons/CubeWeapon.tscn").instantiate())
+	gameMain.weaponsManager.add_weapon(preload("res://Game/Weapons/fist.tscn").instantiate())
 	prints("Weapon", gameMain.weaponsManager.weapons.size(), "/", gameMain.player.resource.stat_weapon_max)
 static func poll(gameMain:GameMain):
 	return 1.0 / float(max(1,gameMain.weaponsManager.weapons.size()))
--- Game/Items/ItemUpgradeGolfBall.gd
+++ Game/Items/ItemUpgradeGolfBall.gd
@@ -8,7 +8,7 @@ func _init():
 	texture = preload("res://Assets/Art/Weapons/Golf Ball/GolfBall.png")
 	base_price = 10
 func apply(gameMain:GameMain):
-	gameMain.weaponsManager.add_weapon(preload("res://Game/Weapons/CubeWeapon.tscn").instantiate())
+	gameMain.weaponsManager.add_weapon(preload("res://Game/Weapons/GolfBallWeapon.tscn").instantiate())
 	prints("Weapon", gameMain.weaponsManager.weapons.size(), "/", gameMain.player.resource.stat_weapon_max)
 static func poll(gameMain:GameMain):
 	return 1.0 / float(max(1,gameMain.weaponsManager.weapons.size()))
