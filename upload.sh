6c73a34032f53aa3f226775bda45173b31428c78
CodeDoes <codedoes@gmail.com>
2026-03-20 22:55:06 +0200

    windows upload too

--- upload.sh
+++ upload.sh
@@ -1,2 +1,4 @@
 godot --export-release "Web"
-butler push ../exports/squap/Web freedom/squap:html5
+godot --export-release "Win"
+butler push ../exports/squap/Web freedom/squap-demo:html5
+butler push ../exports/squap/Win freedom/squap-demo:windows
